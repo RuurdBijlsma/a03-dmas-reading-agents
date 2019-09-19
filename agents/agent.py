bf262f950136b3971eaaa139626f171595e573ea
Jorn van Wier <mail@jornvanwier.com>
2019-09-19 16:24:41 +0200

    replace read_books with set

--- agents/agent.py
+++ agents/agent.py
@@ -7,17 +7,17 @@ class Agent(object):
         self.name = name
         self.energy = 0
         self.choice = 0
-        self.read_books = []
+        self.read_books = set()
 
     def choose_book(self):
         pass
 
     def read(self):
         self.energy += parameters.READ_COST
-        self.read_books.append(self.choice)
+        self.read_books.add(self.choice)
 
     def gossip(self, other: 'Agent'):
         self.energy += parameters.GOSSIP_COST
         # get the union of the read lists
-        self.read_books = list(set(self.read_books) | set(other.read_books))
+        self.read_books = self.read_books | other.read_books
         other.read_books = self.read_books
