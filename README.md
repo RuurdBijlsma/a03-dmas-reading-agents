ea4645af79aa606fe1954a26725bac78c5aee8cd
Ruurd Bijlsma <ruurd@bijlsma.dev>
2019-09-19 16:37:31 +0200

    Add readme, remove .idea folder

---
+++ README.md
@@ -0,0 +1,18 @@
+## Gossip task division simulation
+This program simulates a multi-agent task division problem with gossip.
+###Running the program
+Required python version: 3+
+
+To run the program, execute: 
+
+```python main.py```
+
+### Adjusting the parameters
+Adjust parameters by editing parameters.py
+
+###Collaborators: 
+* Jorn van Wier
+* Ruurd Bijlsma
+* Jorge Anton Garcia
+* Alexandros Charisis
+
--- agents/random_agent.py
+++ agents/random_agent.py
@@ -13,6 +13,7 @@ class RandomAgent(Agent):
         # Don't reread books (random.randint can be used if this rereading is desired) This is too slow!
         # options = [n for n in range(1, parameters.N_BOOKS + 1) if n not in self.read_books]
         # self.choice = random.choice(options)
+
         self.choice = self.next_choice()
         while self.choice in self.read_books:
             self.choice = self.next_choice()
