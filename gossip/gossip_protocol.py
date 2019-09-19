371138818beeaad1beb614274787d410363e4565
Jorn van Wier <mail@jornvanwier.com>
2019-09-19 16:16:52 +0200

    fix typo

--- gossip/gossip_protocol.py
+++ gossip/gossip_protocol.py
@@ -3,7 +3,7 @@ from agents import Agent
 
 class GossipProtocol(object):
     def __init__(self, agent: Agent):
-        self.agent = Agent
+        self.agent = agent
 
     def can_gossip(self, other: Agent):
         pass
