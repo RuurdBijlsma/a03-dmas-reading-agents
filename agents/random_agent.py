7e4f7f4e1c0beb060af0763c265288411394ca9d
Jorn van Wier <mail@jornvanwier.com>
2019-09-19 15:55:56 +0200

    Improve performance of book selection

--- agents/random_agent.py
+++ agents/random_agent.py
@@ -5,7 +5,14 @@ from agents import Agent
 
 
 class RandomAgent(Agent):
+    @staticmethod
+    def next_choice():
+        return random.randint(1, parameters.N_BOOKS)
+
     def choose_book(self):
-        # Don't reread books (random.randint can be used if this rereading is desired)
-        options = [n for n in range(1, parameters.N_BOOKS + 1) if n not in self.read_books]
-        self.choice = random.choice(options)
+        # Don't reread books (random.randint can be used if this rereading is desired) This is too slow!
+        # options = [n for n in range(1, parameters.N_BOOKS + 1) if n not in self.read_books]
+        # self.choice = random.choice(options)
+        self.choice = self.next_choice()
+        while self.choice in self.read_books:
+            self.choice = self.next_choice()
