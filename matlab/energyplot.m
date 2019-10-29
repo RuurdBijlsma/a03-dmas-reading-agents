f811fb54682c3c94570f9e04fdd971c27d338eae
Ruurd Bijlsma <ruurd@bijlsma.dev>
2019-10-29 16:25:17 +0100

    Keep colors consistent over both plots

--- matlab/energyplot.m
+++ matlab/energyplot.m
@@ -6,17 +6,21 @@ function energyplot(plotTitle, fileName, energyColumn)
     iterations = 50
     repeats = 20
     protocols = {"Token", "Spider", "Call me once", "Learn new secret"}
+    colors = {'r','g','b','m'}
     plots = [];
 
 
     hold on;
-    for protocol = protocols
+    for i = 1:4
+        protocol = protocols(i);
+        color = colors(i);
+
         results = csvread(strcat("../data/", protocol{1}, "_", mat2str(iterations), "_", mat2str(repeats), ".csv"), 1, 0);
 
         agents = results(:, 3);
         energy = results(:, energyColumn);
 
-        plots = [plots plot(agents, energy)];
+        plots = [plots plot(agents, energy, color)];
     end
     hold off;
 
